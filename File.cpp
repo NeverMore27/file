7f2eb3c9848d307caf9e1c257051045017bbc00c
NeverMore27 <uragan.uragan.59@mail.ru>
2017-03-18 22:09:35 +0300

    Argc

--- File.cpp
+++ File.cpp
@@ -11,24 +11,24 @@
 
 using namespace std;
 
-void Files(char* name1, char* name2, ofstream &file3);
+void Files(char* name1, char* name2);
 
-void main()
+int main(int argc, char *argv[])
 {
 	
 
-	char n1[raz], n2[raz], n3[raz];
-	cin.getline(n1, raz);
-	cin.getline(n2, raz);
-	cin.getline(n3, raz);
-	ofstream file3(n3);
-	Files(n1, n2, file3);
-	Files(n2, n1, file3);
-	file3.close();
+	char n1[raz], n2[raz];
+	if (argc == 3)
+	{
+		strcpy_s(n1, argv[1]);
+		strcpy_s(n2, argv[2]);
+	}
+	Files(n1, n2);
+	Files(n2, n1);
 	system("pause");
 }
 
-void Files(char* name1, char* name2, ofstream &file3)
+void Files(char* name1, char* name2)
 {
 	ifstream file(name1);
 	ifstream file2(name2);
@@ -58,7 +58,7 @@ void Files(char* name1, char* name2, ofstream &file3)
 		file2.seekg(0, ios::beg);
 		if (i == k)
 		{
-			file3  << name1 << " " << i1 << " " << tmp << endl;
+			cout  << name1 << " " << i1 << " " << tmp << endl;
 		}
 	}
 	file2.close();
--- README.md
+++
@@ -1,1 +0,0 @@
-# nevermore27rep
